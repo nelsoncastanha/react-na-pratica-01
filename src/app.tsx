c3e99b013184d0e6d7487fdd59cf44ed9c3805da
Nelson Castanha <nelsoncastanha@Nelsons-MacBook-Air.local>
2024-02-24 21:59:22 -0300

    feat: list, filters & pagination

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,66 @@
-import { FileDown, Plus, Search, MoreHorizontal } from 'lucide-react';
-import { Header } from './components/header';
-import { Tabs } from './components/tabs';
-import { Button } from './components/ui/button';
-import { Control, Input } from './components/ui/input';
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table';
-import { Pagination } from './components/pagination';
+import { Plus, Search, Filter, FileDown, MoreHorizontal } from 'lucide-react'
+import { Header } from './components/header'
+import { Tabs } from './components/tabs'
+import { Button } from './components/ui/button'
+import { Control, Input } from './components/ui/input'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table'
+import { Pagination } from './components/pagination'
+import { useQuery, keepPreviousData } from '@tanstack/react-query'
+import { useSearchParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import useDebounceValue from './hooks/use-debounce-value'
+
+export interface TagResponse {
+  first: number
+  prev: number | null
+  next: number
+  last: number
+  pages: number
+  items: number
+  data: Tag[]
+}
+
+export interface Tag {
+  title: string
+  amountOfVideos: number
+  id: string
+}
 
 export function App() {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const urlFilter = searchParams.get('filter') ?? ''
+  
+  const [filter, setFilter] = useState(urlFilter)
+
+  const page = searchParams.get('page') ? Number(searchParams.get('page')) : 1
+
+  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+    queryKey: ['get-tags', urlFilter, page],
+    queryFn: async () => {
+      const response = await fetch(`http://localhost:3333/tags?_page=${page}&_per_page=10&title=${urlFilter}`)
+      const data = await response.json()
+
+      // delay 2s
+      await new Promise(resolve => setTimeout(resolve, 2000))
+
+      return data
+    },
+    placeholderData: keepPreviousData,
+  })
+
+  function handleFilter() {
+    setSearchParams(params => {
+      params.set('page', '1')
+      params.set('filter', filter)
+
+      return params
+    })
+  }
+
+  if (isLoading) {
+    return null
+  }
+
   return (
     <div className="py-10 space-y-8">
       <div>
@@ -23,10 +77,20 @@ export function App() {
         </div>
 
         <div className="flex items-center justify-between">
-          <Input variant='filter'>
-            <Search className="size-3" />
-            <Control placeholder="Search tags..." />
-          </Input>
+          <div className="flex items-center">
+            <Input variant='filter'>
+              <Search className="size-3" />
+              <Control 
+                placeholder="Search tags..." 
+                onChange={e => setFilter(e.target.value)}
+                value={filter}
+              />
+            </Input>
+            <Button onClick={handleFilter}>
+              <Filter className="size-3" />
+              Filter
+            </Button>
+          </div>
 
           <Button>
             <FileDown className="size-3" />
@@ -44,31 +108,31 @@ export function App() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {Array.from({ length: 10 }).map((value, index) => {
+            {tagsResponse?.data.map((tag) => {
               return (
-              <TableRow key={index}>
-                <TableCell></TableCell>
-                <TableCell>
-                  <div className="flex flex-col">
-                    <span className="font-medium">React</span>
-                    <span className="text-xs text-zinc-500">c7c731b3-b30a-4383-9097-8872f09b47f3</span>
-                  </div>
-              </TableCell>
-              <TableCell className="text-zinc-300">
-                13 video(s)
-              </TableCell>
-              <TableCell className="text-right">
-                <Button size="icon">
-                  <MoreHorizontal className="size-4" />
-                </Button>
-              </TableCell>
-            </TableRow>
+                <TableRow key={tag.id}>
+                  <TableCell></TableCell>
+                  <TableCell>
+                    <div className="flex flex-col gap-0.5">
+                      <span className="font-medium">{tag.title}</span>
+                      <span className="text-xs text-zinc-500">{tag.id}</span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="text-zinc-300">
+                    {tag.amountOfVideos} video(s)
+                  </TableCell>
+                  <TableCell className="text-right">
+                    <Button size="icon">
+                      <MoreHorizontal className="size-4" />
+                    </Button>
+                  </TableCell>
+                </TableRow>
               )
             })}
           </TableBody>
         </Table>
 
-        <Pagination />
+        {tagsResponse && <Pagination pages={tagsResponse.pages} items={tagsResponse.items} page={page} />}
       </main>
     </div>
   )
